b47cd81135f95efb21d3c3bffa378dd8a147457d
ines <inesqtr@gmail.com>
2019-10-16 17:12:22 +0100

    created new travels page

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Travel from './Travel';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to My Travels!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Travel
+        destination="Viana do Castelo"
+        country="Portugal"
+        image="https://storage.needpix.com/rsynced_images/monastery-santa-luzia-978277_1280.jpg"
+        distance="388km"
+      />
+      <Travel
+        destination="Nyaungshwe"
+        country="Myanmar"
+        image="https://images.unsplash.com/photo-1516527891761-981ae17a53b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        distance="9886km"
+      />
     </div>
   );
 }
---
+++ src/Travel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Travel = ({ destination, country, image, distance }) => (
+    <div>
+        <h1>{destination}</h1>
+        <h2>{country}</h2>
+        <img src={image} />
+        <p>Distance: {distance}</p>
+    </div>
+)
+
+
+export default Travel;
